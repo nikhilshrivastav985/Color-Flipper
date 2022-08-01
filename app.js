aef8ba3ca0cb9ed148cca71f71635c77476a88e8
Nikhil Shrivastav <nikhilshrivastav985@gmail.com>
2022-08-01 22:24:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const hex = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const hexValue = document.querySelector('.hex-value');
-const button = document.querySelector('button');
-const headingText = document.querySelector('.heading');
+// array of hex color code format
+const hexCharacters = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexValue = document.querySelector('.hex-value'); // generated color code
+const button = document.querySelector('button'); //color changer button
+const headingText = document.querySelector('.heading'); 
 
+// generating random no. from 0 to 'hex length'
 function getRandomNumber(){
-    return Math.floor(Math.random() * hex.length);
+    return Math.floor(Math.random() * hexCharacters.length);
 }
 
 function hexValuePicker(){
     let hexColor = '#';
+//     loop for adding six random characters from hex(array) to the hexColor variable
     for( i = 0; i < 6; i++){
-        hexColor += hex[getRandomNumber()]
+        hexColor += hexCharacters[getRandomNumber()]
     }
-    hexValue.style.color = hexColor;
-    hexValue.textContent = hexColor;
-    document.body.style.background = hexColor;
-    headingText.style.color = hexColor;
+    hexValue.style.color = hexColor; // changing Color of generated color code 
+    hexValue.textContent = hexColor; // new color code
+    document.body.style.background = hexColor; // changing page's background color to the color generated
+    headingText.style.color = hexColor; // changing navbar heading color
 }
 
-
+// button which changes color by above 2 functions
 button.addEventListener('click', () => {
     getRandomNumber();
     hexValuePicker();
